a3dbf5e53069afbbf5fbf3278f493ab4e5ef676f
KramarovskayaYE <kramarovskaaana@gmail.com>
2026-02-07 17:34:06 +0300

    Update MainWindow.xaml.cs

--- Group4333/MainWindow.xaml.cs
+++ Group4333/MainWindow.xaml.cs
@@ -7,4 +7,10 @@ namespace Group4333
         public MainWindow()
             => InitializeComponent();
     }
+
+    private void Button_Yana_Click(object sender, RoutedEventArgs e)
+    {
+        var window = new _4333_Kramarovskaya(); 
+        window.Show();
+    }
 }

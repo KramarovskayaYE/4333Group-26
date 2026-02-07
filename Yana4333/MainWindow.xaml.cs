fab330d6c430562f043cb7203335069f609989e8
yana <kramarovskaaana@gmail.com>
2026-02-07 17:02:57 +0300

    Добавлен обработчик события для кнопки

--- Yana4333/MainWindow.xaml.cs
+++ Yana4333/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Yana4333
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
